06eefc41a6a249833457cc37e2247f7c48e21a4d
stern15 <ssemasuka@gmail.com>
2016-03-21 14:58:30 +0300

    adding the camera app

--- app/src/main/java/com/example/onikah/capture/MainActivity.java
+++ app/src/main/java/com/example/onikah/capture/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.onikah.capture;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends Activity {
+    Button button;
+    ImageView imageView;
+    static final int CAM_REQUEST=1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.button);
+        imageView=(ImageView)findViewById(R.id.image_view);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent camera_intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file=getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent,CAM_REQUEST);
+
+
+            }
+        });
+    }
+    private File getFile(){
+        File folder=new File("android/camera_app");
+        if(!folder.exists()){
+
+            folder.mkdir();
+        }
+
+        File image_file=new File(folder,"cam_image.jpg");
+        return image_file;
     }
 }
