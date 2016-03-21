195b1f94f4b35f7b1d576d66bf420b84fea11a81
Onikah <mona.kwamboka@gmail.com>
2016-03-21 10:31:37 +0300

    splash activity java

---
+++ app/src/main/java/com/example/onikah/capture/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.onikah.capture;
+
+/**
+ * Created by onikah on 3/21/16.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+
+public class SplashScreen extends Activity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+//5000 milli seconds are delaying time to redirect to NextActivity
+        mHandler.sendEmptyMessageDelayed(0, 5000);
+
+    }
+
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            startActivity(new Intent(SplashScreen.this, GalleryActivity.class));
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            finish();
+        }
+
+        ;
+
+    };
+}
